e8a19e503adea613ae118f4ddf1f9bfe1259bb52
rokopluk <130516150+rokopluk@users.noreply.github.com>
2024-03-11 21:36:27 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,7 @@
+module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy("images");
+};
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("styles.css");
+  };
